f9d3cc59a5f6e3ee00b0bdc41dede8a72b45070d
Christopher Maes <cmaes@mit.edu>
2011-10-28 00:40:35 -0400

    Fix for camera direction

--- hyperplane_projection.m
+++ hyperplane_projection.m
@@ -7,9 +7,19 @@ function [P, Z] = hyperplane_projection(a)
 %  { x in R^n |  a'*x == 0 } 
 %
 
-
-
 Z = null(a');
+
+z1 = Z(:,1);
+z2 = Z(:,2); 
+
+if a'*cross(z1,z2) < 0 
+   z1 = -z1;
+end
+
+Z(:,1) = z1;
+Z(:,2) = z2;
+
+
 % P = Z(Z'*Z)Z' really but null returns a matrix Z whose columns
 % are orthonormal so Z'*Z = I
 P =  Z*Z';   
